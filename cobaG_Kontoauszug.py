60906fe64b3ad29188240234cab05776e0c30d9f
FlorianKr02 <155002068+FlorianKr02@users.noreply.github.com>
2024-01-24 17:48:44 +0100

    Update cobaG_Kontoauszug.py

--- cobaG_Kontoauszug.py
+++ cobaG_Kontoauszug.py
@@ -12,6 +12,8 @@ def import_CobaGiro(file_path):
                 empfaenger = verwendungszweck.split()[0]
             elif float(betrag.replace(',','.')) > 0:
                 empfaenger = "ich"
+            verwendungszweck = re.split(r'\d', verwendungszweck, 1)[0].strip()
             bank_data.append({'Buchungsdatum': buchungsdatum, 'Betrag': betrag, 'Verwendungszweck': verwendungszweck, 'Empfänger': empfaenger})
 
     return bank_data
+
