806fa92ebb6f040620ddcfe0c5b01338e6cb4d7f
FlorianKr02 <155002068+FlorianKr02@users.noreply.github.com>
2024-01-24 17:40:21 +0100

    Create cobaG_Kontoauszug.py

---
+++ cobaG_Kontoauszug.py
@@ -0,0 +1,17 @@
+def import_CobaGiro(file_path):
+    bank_data = []
+
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+
+        for row in reader:
+            buchungsdatum = row['\ufeffBuchungstag']
+            betrag = row['Betrag']
+            verwendungszweck = row['Buchungstext']
+            if float(betrag.replace(',','.')) < 0:
+                empfaenger = verwendungszweck.split()[0]
+            elif float(betrag.replace(',','.')) > 0:
+                empfaenger = "ich"
+            bank_data.append({'Buchungsdatum': buchungsdatum, 'Betrag': betrag, 'Verwendungszweck': verwendungszweck, 'Empfänger': empfaenger})
+
+    return bank_data
