c81d4bf6bcfa0431ebfe298e25e11dc726b293d0
FlorianKr02 <155002068+FlorianKr02@users.noreply.github.com>
2024-02-04 19:52:06 +0100

    Update kategorisierung.py

--- kategorisierung.py
+++ kategorisierung.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def load_keywords_from_csv(file_path):
     kategorien = {}
     with open(file_path, newline='', encoding='utf-8') as csvfile:
@@ -14,9 +15,22 @@ def kategorisiere_transaktion(verwendungszweck, betrag, empfänger, kategorien):
     verwendungszweck_klein = verwendungszweck.lower()
     empfaenger_klein = empfänger.lower()
     oberkategorie = 'Einnahmen' if betrag > 0 else 'Ausgaben'
+
     for kategorie, data in kategorien.items():
+
         if any(keyword in verwendungszweck_klein for keyword in data['keywords']):
+
             return kategorie, oberkategorie
+        
         if any(keyword in empfaenger_klein for keyword in data['keywords']):
+
             return kategorie, oberkategorie
-    return 'Sonstiges', oberkategorie
+        
+# Wenn keine passende Kategorie gefunden wurde, überprüft Oberkategorie
+    if oberkategorie == 'Ausgaben':
+        return 'Sonstige Ausgaben', oberkategorie
+    elif oberkategorie == 'Einnahmen':
+        return 'Sonstige Einnahmen', oberkategorie
+
+    # Wenn weder Kategorie noch Oberkategorie übereinstimmen, fällt es in "Sonstiges"
+    return 'Sonstiges', oberkategorie        
