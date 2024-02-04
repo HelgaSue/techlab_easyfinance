693676c6ec982027f95311ff190f1288b39b6c29
FlorianKr02 <155002068+FlorianKr02@users.noreply.github.com>
2024-02-04 12:10:50 +0100

    Create CobaG_kontoauszug.py

---
+++ CobaG_kontoauszug.py
@@ -0,0 +1,43 @@
+import csv
+import re
+
+
+def import_cobag():
+    file_path = r"C:\Users\Florian\OneDrive\Techlabs 2023\CoBa.csv"
+    bank_data = []
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            buchungsdatum = row['\ufeffBuchungstag']
+            betrag = float(row['Betrag'].replace(',', '.'))
+            verwendungszweck = row['Buchungstext']
+            empfaenger = "ich" if betrag > 0 else re.split(r'\d', verwendungszweck, 1)[0].strip()
+           
+            modified_verwendungszweck = extract_modified_purpose(verwendungszweck)
+            values_list = [buchungsdatum, betrag, modified_verwendungszweck, empfaenger] # oberkategorie, kategorie
+            bank_data.append(values_list)
+        return bank_data
+    
+def extract_modified_purpose(text):
+    # Schneidet den Text an der Stelle "End-to-End-Ref" ab, falls vorhanden
+    end_to_end_ref_index = text.find('End-to-End-Ref')
+    if end_to_end_ref_index != -1:
+        text_before_end_to_end = text[:end_to_end_ref_index].strip()
+        # Extrahiert den Text nach der letzten Zahl vor "End-to-End-Ref"
+        match = re.search(r'\d+(.*)', text_before_end_to_end)
+        if match:
+            after_number_to_end_to_end = match.group(1).strip()
+            # Überprüfen, ob der Text zwischen der ersten Zahl und "End-to-End-Ref" nur aus Zahlen besteht
+            if re.search('[a-zA-Z]', after_number_to_end_to_end):
+                # Wenn Buchstaben vorhanden sind, verwenden Sie diesen Teil
+                return after_number_to_end_to_end
+            else:
+                # Wenn keine Buchstaben vorhanden sind, verwenden Sie den Text vor der ersten Zahl
+                return text_before_end_to_end[:match.start()].strip()
+        else:
+            return text_before_end_to_end
+    else:
+        # Wenn "End-to-End-Ref" nicht vorhanden ist, verwenden Sie den Text vor der ersten Zahl
+        match = re.split(r'\d', text, 1)
+        return match[0].strip() if match else text
+sorted_statement = import_cobag()
